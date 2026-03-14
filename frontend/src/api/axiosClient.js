7a404f7190c1799f3cd719cbc1c208c897e5cf59
Nguyen Huu Dat <nguyenhuudat@192.168.1.11>
2026-03-14 13:32:28 +0700

    add minimum loading time 2

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "https://small-business-management-system.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
